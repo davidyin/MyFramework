b98d93490f7490cde20818cbe8224b30a7ef40ca
Bingwei Yin <david@local>
2017-12-16 09:08:31 -0500

    Finished our myframework

---
+++ MyFramework/MyFramework/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by Bingwei Yin on 2017-12-16.
+//  Copyright © 2017 Roxware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
---
+++ MyFramework/MyFramework/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  MyFramework
+//
+//  Created by Bingwei Yin on 2017-12-16.
+//  Copyright © 2017 Roxware. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    public static func doSomething() -> String {
+        return "Did Some Stuff, here you go!"
+    }
+}
